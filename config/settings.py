598ac894ca1a0c1fc2af6c290be40860f53ab31e
bouba626dev <boubadev626@gmail.com>
2026-09-18 20:19:42 +0000

    Servir les fichiers statiques avec WhiteNoise

--- config/settings.py
+++ config/settings.py
@@ -149,8 +149,16 @@ USE_TZ = True
 
 # --- Fichiers statiques (CSS, JS, images) ---
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
